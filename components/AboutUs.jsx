e0599e463c42bed4982384a1ccd77d36766d5dd1
mahbubsk <skmahbubkr787@gmail.com>
2022-07-31 13:18:54 +0600

    Feature-added: text added on about us section and changed font-family

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -1,26 +1,38 @@
 import AboutUsCard from "./AboutUsCard";
-import { Grid, Text } from "@nextui-org/react";
+import { Grid, Spacer, Text } from "@nextui-org/react";
 
 const Service = () => {
     return (
         <div>
-             <Text
+            <Text
                 h1
                 size={60}
                 css={{
                     textGradient: "135deg, #fdd819 10%, #e80505 100%",
+                    fontFamily: "walter"
                 }}
                 style={{textAlign:"center"}}
                 >
                 About US
             </Text>
+            <Spacer y={1}/>
+
+            <Text 
+                css={{
+                    width:"60%",
+                    margin:"auto",
+                    color:"#424242"
+                }}
+                h4>
+                Carave Software Solutions is a software development company that serves across the world. We use technology for solving your business problems. EPR, WordPress, POS, E-commerce, Development, Deployment,  and Management of any kind of Problem.
+            </Text>
+            <Spacer y={1}/>
             <Grid.Container gap={4} justify="center">
                 <Grid xs={12} sm={3}>
                     <AboutUsCard mail="imranhasan871@gmail.com" designation="Founder" src="/imran.jpg" name="Imran Hasan"/>
                 </Grid>
                 <Grid xs={12} sm={3}>
                     <AboutUsCard mail="skmahbubkr787@gmail.com" designation="Co-Founder" src="/mahbub.jpg" name="Khandakar Mahbub"/>
-                    
                 </Grid>
                 
             </Grid.Container>
