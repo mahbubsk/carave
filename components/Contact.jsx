bf030b296089e2818233befc3b0ccc85299a30c7
mahbubsk <skmahbubkr787@gmail.com>
2022-08-03 00:08:58 +0600

    Contact text added

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,19 @@
+import { Text } from "@nextui-org/react";
 const Contact = () => {
     return (
         <div>
+            <Text
+                h1
+                size={60}
+                css={{
+                    textGradient: "135deg, #fdd819 10%, #e80505 100%",
+                    fontFamily: "walter"
+                }}
+                style={{textAlign:"center"}}
+                >
+                Contact
+            </Text>
+
             
         </div>
     );
