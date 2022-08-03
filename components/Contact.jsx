ea392cab3c6b96bade8af08da5759bc96cd11d42
mahbubsk <skmahbubkr787@gmail.com>
2022-08-03 22:43:23 +0600

    contact section done for v1.0

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,8 @@
-import { Text } from "@nextui-org/react";
+import { Text, Image, Row, Col, Spacer } from "@nextui-org/react";
+import {ImLocation} from "react-icons/im";
+import {SiGmail} from "react-icons/si";
+import {BsTelephoneFill} from "react-icons/bs";
+
 const Contact = () => {
     return (
         <div>
@@ -13,7 +17,97 @@ const Contact = () => {
                 >
                 Contact
             </Text>
-
+            
+            <Row>
+                <Col>
+                    <Image
+                        
+                        width="470px"
+                        src="/contact.svg"
+                        alt="Contact Image"
+                        title="Contact Image"
+                        objectFit="cover"
+                    />
+                </Col>
+                <Col
+                    
+                >
+                    <Spacer y={3}/>
+                    <Row
+                        css={{
+                            display:"flex",
+                            alignItems:"center",
+                            color:"#424242"
+                        }}
+                    >
+                        <ImLocation
+                            style={{
+                                fontSize:"1.5rem",
+                                color:"#005fcc"
+                            }}
+                        />
+                        <Text
+                            css={{
+                                marginLeft:"0.8rem",
+                                fontSize:"1.2rem"
+                            }}
+                        >
+                            Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016
+                        </Text>
+                    </Row>
+                    <Spacer/>
+                    <Row 
+                        css={{
+                            display:"flex",
+                            alignItems:"center",
+                            color:"#424242"
+                        }}
+                    >
+                        <BsTelephoneFill
+                            style={{
+                                fontSize:"1.5rem",
+                                color:"#005fcc"
+                            }}
+                        />
+                        <Text
+                            css={{
+                                marginLeft:"0.5rem",
+                                fontSize:"1.2rem"
+                            }}
+                        >
+                            +880 00000 000000,
+                            +880 00000 000000
+                        </Text>
+                        
+                    </Row>
+                    <Spacer/>
+                    <Row 
+                        css={{
+                            display:"flex",
+                            alignItems:"center",
+                            color:"#424242"
+                        }}
+                    >
+                        <SiGmail
+                            style={{
+                                fontSize:"1.5rem",
+                                color:"#005fcc"
+                            }}
+                        />
+                        <Text
+                            css={{
+                                marginLeft:"1rem",
+                                fontSize:"1.2rem"
+                            }}
+                        >
+                            carave-soft@gmail.com
+                            
+                        </Text>
+                        
+                    </Row>
+                </Col>
+                
+            </Row>
             
         </div>
     );
