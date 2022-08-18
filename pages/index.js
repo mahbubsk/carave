f6d4f87bf6212f10486f9d9dcbb1da5a09d3a324
mahbubsk <skmahbubkr787@gmail.com>
2022-08-18 23:47:11 +0600

    loading feature added successfully

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import { useState, useEffect } from 'react';
+
 import Head from 'next/head'
 import Image from 'next/image'
-import { Container, Switch, Row, Text, Spacer, Button, useTheme, changeTheme } from "@nextui-org/react";
-
+import { Container, Switch, Loading, Spacer, Text } from "@nextui-org/react";
 
 import Nav from "../components/Nav";
 import Hero from "../components/Hero";
@@ -20,14 +21,13 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 export default function Home() {
 
-    // const { type, isDark } = useTheme();
-    // console.log(isDark);
-    // const handleChange = () => {
-    //     const nextTheme = isDark ? 'light' : 'dark';
-    //     window.localStorage.setItem('data-theme', nextTheme); // you can use any storage
-    //     changeTheme(nextTheme);
-    // }
-
+    const [isLoading, setIsLoading] = useState(true);
+    // const loadingMessage = ;
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 3000)
+    }, [isLoading])
 
 
     return (
@@ -42,44 +42,44 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main>
-                <Nav />
-                <Container>
-                    <Spacer y={1} />
-                    <Hero />
-                    <Spacer y={3} />
-                    <hr />
-                    <Spacer y={3} />
-                    <AboutUs />
-                    <Spacer y={3} />
-                    <Pricing />
-                    <Spacer y={3} />
-                    <hr />
-                    <Review />
-                    <Spacer y={3} />
-                    <hr />
-                    <Success />
-                    <Spacer y={3} />
-                    <hr />
-                    <Contact />
-                    <Spacer y={3} />
-                    <CompanyLogos />
-                    <Spacer y={3} />
-                </Container>
-                <MessengerCustomerChat
-                    pageId="315825732576242"
-                    appId="906769103521842"
-                />
+            {
+                isLoading ?
+                    <Text css={{ display: "flex", ai: "center", jc: "center", width: "100%", height: "100vh" }} >
+                        <Loading type="spinner" size="xl" />
+                    </Text>
+                    :
+                    <main>
+                        <Nav />
+                        <Container>
+                            <Spacer y={1} />
+                            <Hero />
+                            <Spacer y={3} />
+                            <hr />
+                            <Spacer y={3} />
+                            <AboutUs />
+                            <Spacer y={3} />
+                            <Pricing />
+                            <Spacer y={3} />
+                            <hr />
+                            <Review />
+                            <Spacer y={3} />
+                            <hr />
+                            <Success />
+                            <Spacer y={3} />
+                            <hr />
+                            <Contact />
+                            <Spacer y={3} />
+                            <CompanyLogos />
+                            <Spacer y={3} />
+                        </Container>
+                        <MessengerCustomerChat
+                            pageId="315825732576242"
+                            appId="906769103521842"
+                        />
 
-                <Footer />
-                <div>
-                    The current theme is: {type}
-                    <Switch
-                        checked={isDark}
-                        onChange={handleChange}
-                    />
-                </div>
-            </main>
+                        <Footer />
+                    </main>
+            }
         </div>
     )
 }
